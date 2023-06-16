1abeb0a9f9de1eaca3c6a2057de9f6998932bdc9
daniel <danavarro63.ds@gmail.com>
2023-06-16 22:51:48 +0100

    fixed error

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ import Image from 'next/image';
 import Navbar from './components/navbar';
 import HeroImage from './components/HeroImage';
 import ExtraDropdowns from './components/extraDropdowns';
-import Products from './products.json';
 
 export default function Home() {
   return (
