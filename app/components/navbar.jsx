c6f24bdc473968c6c72409ada1ac61986fb4ac12
daniel <danavarro63.ds@gmail.com>
2023-06-21 13:25:19 +0100

    x

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,5 +1,4 @@
-"use client";
-import React, { useState,useId } from 'react';
+import React, { useState, useId } from 'react';
 import Freeshipping from './freeshipping';
 import { BsBag } from 'react-icons/bs';
 import { RiMenu4Line, RiCloseLine } from 'react-icons/ri';
@@ -23,16 +22,16 @@ function Navbar({ basketItemsIds, setBasketItemsIds }) {
   const [basketOpen, setIsBasketOpen] = useState(false);
 
   const toggleBasket = () => {
-    if(basketItemsIds == null){
+    if (basketItemsIds == null) {
       return;
     }
-    if(basketItemsIds.length < 1 && basketOpen==false){
+    if (basketItemsIds.length < 1 && basketOpen === false) {
       return;
     }
     setIsBasketOpen(!basketOpen);
   };
 
-  const togglehamburger = () => {
+  const toggleHamburger = () => {
     setIsMenuOpen(!menuOpen);
   };
 
@@ -57,8 +56,8 @@ function Navbar({ basketItemsIds, setBasketItemsIds }) {
 
   function calculateTotal() {
     let total = 0;
-    if(!basketItemsIds){
-      return;
+    if (!basketItemsIds) {
+      return '0.00';
     }
     for (let i = 0; i < basketItemsIds.length; i++) {
       total = total + Products[basketItemsIds[i]].price;
@@ -70,42 +69,41 @@ function Navbar({ basketItemsIds, setBasketItemsIds }) {
     <>
       <Freeshipping />
       <div className='h-24 items-center justify-between flex mx-4'>
-      <div> 
-        <Link href="/">
-          <Image className='cursor-pointer' src={Logo} alt={"logo"} width={100} height={100} />
-        </Link>
-      </div>
-      <div>
-      <div className='flex flex-row items-center bg-gray-100 rounded-2xl h-10 px-2 justify-between lg:w-96 md:w-72 sm:w-60 xs:w-48 w-40'>
-              <Select
-                options={options}
-                value={searchQuery}
-                onChange={(selectedOption) => {
-                  setSearchQuery(selectedOption);
-                  handleSearch();
-                }}
-                onKeyDown={handleKeyDown}
-                placeholder="Search"
-                isClearable
-                isSearchable
-                className="w-full rounded-lg focus:outline-none outline-none text-sm"
-                classNamePrefix="react-select"
-                instanceId={useId()}
-                styles={{
-                  dropdownIndicator: (provided) => ({
-                    ...provided,
-                    display: 'none',
-                    boxShadow: 'none',
-                    zIndex: 20,
-                    outline: 'none',
-
-                  }),
-                }}
-              />
-              <button onClick={handleSearch} className='text-sm'>Search</button>
+        <div>
+          <Link href="/">
+            <Image className='cursor-pointer' src={Logo} alt={"logo"} width={100} height={100} />
+          </Link>
         </div>
-      </div>
-      <div className='hidden md:flex md:flex-row md:gap-4 items-center'>
+        <div>
+          <div className='flex flex-row items-center bg-gray-100 rounded-2xl h-10 px-2 justify-between lg:w-96 md:w-72 sm:w-60 xs:w-48 w-40'>
+            <Select
+              options={options}
+              value={searchQuery}
+              onChange={(selectedOption) => {
+                setSearchQuery(selectedOption);
+                handleSearch();
+              }}
+              onKeyDown={handleKeyDown}
+              placeholder="Search"
+              isClearable
+              isSearchable
+              className="w-full rounded-lg focus:outline-none outline-none text-sm resize-none pointer-events-none"
+              classNamePrefix="react-select"
+              instanceId={useId()}
+              styles={{
+                dropdownIndicator: (provided) => ({
+                  ...provided,
+                  display: 'none',
+                  boxShadow: 'none',
+                  zIndex: 20,
+                  outline: 'none',
+                }),
+              }}
+            />
+            <button onClick={handleSearch} className='text-sm'>Search</button>
+          </div>
+        </div>
+        <div className='hidden md:flex md:flex-row md:gap-4 items-center'>
           <div className='flex flex-row gap-2 items-center cursor-pointer hover:text-black/75'>
             <button onClick={toggleBasket}>Basket/£{calculateTotal()}</button>
             <BsBag onClick={toggleBasket} />
@@ -115,18 +113,17 @@ function Navbar({ basketItemsIds, setBasketItemsIds }) {
           </Link>
         </div>
         <div className="relative mr-4 md:hidden ">
-  <BsBag size={40} className="md:hidden cursor-pointer" onClick={toggleBasket} />
-  {
-    basketItemsIds.length > 0 && 
-  <p className="md:hidden flex bg-red-500 text-white rounded-full w-5 h-5 items-center justify-center absolute -top-2 -left-2 text-xs">
-    {basketItemsIds.length}
-  </p>
-  }
-</div>
+          <BsBag size={40} className="md:hidden cursor-pointer" onClick={toggleBasket} />
+          {basketItemsIds.length > 0 &&
+            <p className="md:hidden flex bg-red-500 text-white rounded-full w-5 h-5 items-center justify-center absolute -top-2 -left-2 text-xs">
+              {basketItemsIds.length}
+            </p>
+          }
+        </div>
       </div>
 
       {basketOpen &&
-        <Basket  basketItemsIds={basketItemsIds} setBasketItemsIds={setBasketItemsIds} />}
+        <Basket basketItemsIds={basketItemsIds} setBasketItemsIds={setBasketItemsIds} />}
     </>
   );
 }
